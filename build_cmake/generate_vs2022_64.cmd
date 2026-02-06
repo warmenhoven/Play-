b75abf3f5638df99d69fd9169a1a46bd325c98c0
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2026-02-06 16:59:59 -0500

    Update scripts.

--- build_cmake/generate_vs2022_64.cmd
+++ build_cmake/generate_vs2022_64.cmd
@@ -1,2 +1,2 @@
 @echo off
-cmake .. --preset windows-x86_64-vs -DCMAKE_PREFIX_PATH="C:\Qt\6.8.0\msvc2022_64"
+cmake .. --preset windows-x86_64-vs -DCMAKE_PREFIX_PATH="C:\Qt\6.10.2\msvc2022_64" --fresh
